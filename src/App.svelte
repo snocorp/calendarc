108d08aaf97d9013d791a99670dc053dc760c3d5
Dave Sewell <snocorp@gmail.com>
2021-05-24 18:01:32 -0400

    Highlight today and other fixes

--- src/App.svelte
+++ src/App.svelte
@@ -1,70 +1,74 @@
 <script>
-  import { DateTime, Duration, Info } from "luxon";
+  import { DateTime, Duration, Info } from 'luxon'
 
   function getLang() {
-    if (navigator.languages != undefined) return navigator.languages[0];
-    else return navigator.language;
+    if (navigator.languages != undefined) return navigator.languages[0]
+    else return navigator.language
   }
 
   function getMonthGrid(date) {
-    const grid = [];
-    const first = date.startOf("month");
-    const end = first.endOf("month").endOf("week");
-    let curr = first.startOf("week");
-    const count = Math.round(end.diff(curr).as("days"));
-    let index = 0;
-    let week = 0;
-    let weekday;
+    const grid = []
+    const first = date.startOf('month')
+    const end = first.endOf('month').endOf('week')
+    let curr = first.startOf('week')
+    const count = Math.round(end.diff(curr).as('days'))
+    let index = 0
+    let week = 0
+    let weekday
     while (index < count) {
       if (curr.weekday === 1) {
-        grid.push(new Array(7));
-        week = grid.length - 1;
-        weekday = 0;
+        grid.push(new Array(7))
+        week = grid.length - 1
+        weekday = 0
       }
 
       grid[week][weekday] = {
-        date: curr,
-      };
+        date: curr
+      }
 
-      curr = curr.plus(Duration.fromObject({ days: 1 }));
-      weekday++;
-      index++;
+      curr = curr.plus(Duration.fromObject({ days: 1 }))
+      weekday++
+      index++
     }
 
-    return grid;
+    return grid
   }
 
   function update() {
-    weeks = getMonthGrid(date);
+    weeks = getMonthGrid(date)
 
-    const lastMonth = date.minus({ months: 1 });
-    lastMonthFormatted = lastMonth.toFormat("MMMM yyyy");
+    const lastMonth = date.minus({ months: 1 })
+    lastMonthFormatted = lastMonth.toFormat('MMMM yyyy')
 
-    const nextMonth = date.plus({ months: 1 });
-    nextMonthFormatted = nextMonth.toFormat("MMMM yyyy");
+    const nextMonth = date.plus({ months: 1 })
+    nextMonthFormatted = nextMonth.toFormat('MMMM yyyy')
 
-    monthFormatted = date.toFormat("MMMM yyyy");
+    monthFormatted = date.toFormat('MMMM yyyy')
   }
 
   function handleLastMonthClick() {
-    date = date.minus({ months: 1 });
-    update();
+    date = date.minus({ months: 1 })
+    update()
   }
 
   function handleNextMonthClick() {
-    date = date.plus({ months: 1 });
-    update();
+    date = date.plus({ months: 1 })
+    update()
   }
 
-  export const weekdaysFormatted = Info.weekdays("short");
-  export let date = DateTime.local();
+  function isToday(otherDate) {
+    return DateTime.local().startOf('day').equals(otherDate.startOf('day'))
+  }
 
-  export let weeks = [];
-  export let lastMonthFormatted = "";
-  export let nextMonthFormatted = "";
-  export let monthFormatted = "";
+  export const weekdaysFormatted = Info.weekdays('short')
+  export let date = DateTime.local()
 
-  update();
+  export let weeks = []
+  export let lastMonthFormatted = ''
+  export let nextMonthFormatted = ''
+  export let monthFormatted = ''
+
+  update()
 </script>
 
 <main>
@@ -74,10 +78,10 @@
     </div>
     <div class="controls">
       <button class="change-month" on:click={handleLastMonthClick}
-        >{lastMonthFormatted}</button
+        >&laquo; {lastMonthFormatted}</button
       >
       <button class="change-month" on:click={handleNextMonthClick}
-        >{nextMonthFormatted}</button
+        >{nextMonthFormatted} &raquo;</button
       >
     </div>
   </div>
@@ -87,7 +91,9 @@
     {/each}
     {#each weeks as week}
       {#each week as day}
-        <div class="calendar-day">{day.date.toFormat("d")}</div>
+        <div class="calendar-day" class:calendar-today={isToday(day.date)}>
+          {day.date.toFormat('d')}
+        </div>
       {/each}
     {/each}
   </div>
